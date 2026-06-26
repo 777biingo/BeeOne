22a551c6b882e45df78a705e2894e2faf9ba2c6a
777biingo <00ch4osek12@gmail.com>
2026-06-26 19:50:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,37 @@
 require('dotenv').config();
-const { Client, GatewayIntentBits } = require('discord.js');
-const { connectDB, getDb } = require('./db');
+const { Client, GatewayIntentBits, EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const { registerCommands } = require('./commands/register');
+const { handleButtonInteraction } = require('./handlers/buttonHandler');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.DirectMessages,
+  ],
+});
 
 client.once('ready', async () => {
-    await connectDB();
-    console.log(`Zalogowano jako ${client.user.tag}`);
+  console.log(`✅ Bot zalogowany jako ${client.user.tag}`);
+  await registerCommands(client);
 });
 
-client.on('messageCreate', async (message) => {
-    if (message.author.bot || !message.content.startsWith('!')) return;
-    
-    const db = getDb();
-    const userId = message.author.id;
-    const args = message.content.split(' ');
-    const command = args[0];
-
-    // Pobranie profilu
-    let user = await db.collection('users').findOne({ userId });
-    if (!user) {
-        user = { userId, honey: 0, bees: 1, honeyPrice: 10 };
-        await db.collection('users').insertOne(user);
+client.on('interactionCreate', async (interaction) => {
+  if (interaction.isCommand()) {
+    if (interaction.commandName === 'start') {
+      const { createStatsEmbed, createButtonRow } = require('./utils/embed');
+      const { getUserStats } = require('./utils/stats');
+      
+      const userId = interaction.user.id;
+      getUserStats(userId);
+      
+      const embed = createStatsEmbed(userId);
+      await interaction.reply({ embeds: [embed], components: [createButtonRow()], ephemeral: false });
     }
-
-    if (command === '!zbieraj') {
-        const gain = Math.floor(Math.random() * 5 * user.bees) + 1;
-        await db.collection('users').updateOne({ userId }, { $inc: { honey: gain } });
-        message.reply(`🐝 Zebrałeś ${gain} miodu!`);
-    }
-
-    if (command === '!sklep') {
-        message.reply(`💰 Stan konta: ${user.honey} miodu. Koszt nowej pszczoły: ${user.bees * 50}. Komenda: !ulepsz`);
-    }
-
-    if (command === '!ulepsz') {
-        const cost = user.bees * 50;
-        if (user.honey < cost) return message.reply("Nie masz wystarczająco miodu!");
-        await db.collection('users').updateOne({ userId }, { $inc: { honey: -cost, bees: 1 } });
-        message.reply(`✅ Ulepszono! Masz teraz ${user.bees + 1} pszczół.`);
-    }
+  } else if (interaction.isButton()) {
+    await handleButtonInteraction(interaction);
+  }
 });
 
-client.login(process.env.TOKEN);
+client.login(process.env.DISCORD_TOKEN);
